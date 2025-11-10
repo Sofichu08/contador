287442d92c168eaa4425e8ae4d46c6a416b947db
Sofichu08 <sofiso.beltran08@gmail.com>
2025-11-10 10:37:10 -0300

    version_final

---
+++ form-prueba2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_prueba2
+{
+    public partial class Form1 : Form
+    {
+        int contador = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            contador++;
+            label1.Text = contador.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            contador--;
+            label1.Text = contador.ToString();
+        }
+    }
+}
